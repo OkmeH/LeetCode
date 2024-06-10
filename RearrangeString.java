d23390aed38f6e6bc31dbcf6fb057c739c3e7f29
OkmeH <172271459+OkmeH@users.noreply.github.com>
2024-06-10 15:29:17 +0200

    Update RearrangeString.java
    Works now for normal alphabet!

--- RearrangeString.java
+++ RearrangeString.java
@@ -1,88 +1,93 @@
 
 import java.util.*;
-class KeyComarator implements Comparator<Key> {
-    //overwrite compare() method
-    public int compare(Key k1, Key k2){
-        if(k1.freq < k2.freq){
+ 
+class KeyComparator implements Comparator<Key> {
+ 
+    //Overwriteing compare()method
+    public int compare(Key k1, Key k2)
+    {
+        if (k1.freq < k2.freq)
             return 1;
-        }else if(k1.freq > k2.freq){
+        else if (k1.freq > k2.freq)
             return -1;
-        }
-        else{
-            return 0;
-        }
+        return 0;
     }
 }
-class Key{
-    int freq;// stores the frequency of the char
+ 
+class Key {
+ 
+    int freq; //frequency of character
     char ch;
-    Key(int val, char c){
+    Key(int val, char c)
+    {
         freq = val;
-        ch=c;
+        ch = c;
     }
 }
-class Rearrange{
-    static int MAX_CHAR = 26;// there are 26 character in the normal alphabet
-
-    //function to rearrange chars of a String so no char repeats twice
-    static void rearrangeString(String str){
-        int length = str.length();
-
-        //Store freq of all chars, of the str, in an arr
+ 
+class RearrangeString {
+    static int MAX_CHAR = 26;
+ 
+    // Function to rearrange character of a string so that no char repeat twice
+    static void rearrangeString(String str)
+    {
+        int n = str.length();
+ 
+        // Store frequencies of all characters in string, in arr
         int[] count = new int[MAX_CHAR];
-
-        for(int i=0;i<length;i++){
+ 
+        for (int i = 0; i < n; i++)
             count[str.charAt(i) - 'a']++;
+ 
+        // Insert all chars with their freq into a pq
+        PriorityQueue<Key> pq = new PriorityQueue<>(new KeyComparator());
+        for (char c = 'a'; c <= 'z'; c++) {
+            int val = c - 'a';
+            if (count[val] > 0){
+                pq.add(new Key(count[val], c));
+            }
         }
-    
-    //Insert all characters with their frequencies in the PQ
-    PriorityQueue<Key> pq = new PriorityQueue<>(new KeyComarator());
-    for(char c ='a';c<='z';c++){
-        int val = c -'a';
-        if(count[val]>0){
-            pq.add(new Key(count[val],c));
-        }
-        //store the resultant string
-        String result ="";
+ 
+        // 'str' that will store resultant value
+        str = "";
+ 
+        // work as the previous visited element, init with default val
 
-        // prev Key, initail values as defult
         Key prev = new Key(-1, '#');
-
-        //traverse pq
-        while (pq.size()!=0){
-
-            //pop top ele frome queue and ad it to the result string
+ 
+        // traverse queue
+        while (pq.size() != 0) {
+ 
+            // pop top element from queue and add it to string.
             Key k = pq.peek();
             pq.poll();
-            result = result + k.ch;
-
-            //If frequency of prev char is less than 0 , its useless
-            if(prev.freq >0){
+            str = str + k.ch;
+ 
+            // If frequency of previous character is less than zero == useless
+            if (prev.freq > 0){
                 pq.add(prev);
             }
-
-            // make current char as prev char and decrease freq by 'one'
+ 
+            // make current character as the previous 'char' decrease frequency by 'one'
             (k.freq)--;
-            prev=k;
+            prev = k;
         }
-
-        //if result length isnt equal to the original string, a rearrangement cant be found
-        if(length != result.length()){
-            System.out.println("No possible rearrangement!");
-        }
-        else{
-            System.out.println(result);
-        }
+ 
+        // If length of the resultant string
+        // and original is not the same, the string is not valid
+        if (n != str.length())
+            System.out.println("No rearrangement possible!");
+        else
+            System.out.println(str);
     }
-
-}
-public static void main(String args[]){
-    Scanner input = new Scanner(System.in);
-
-    String str= input.nextLine();
-    input.close();
-    //System.out.println(str);
-    rearrangeString(str);
-
-}
+ 
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+        input.close();
+ 
+        
+        rearrangeString(str);
+    }
 }
